73e83a6a5ebc856242ccbc397615e9dc6b181ef3
Billy Lowry <billyn.lowry@gmail.com>
2023-03-27 10:41:00 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// Define saturday fun day and use default activity when no arguments are passed and permits default activity to be overridden.
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+// Same as above.
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+
+} 
+// 
+function wrapAdjective(string="*"){
+    return function(parameter="special"){
+        return `You are ${string}${parameter}${string}!`
+    }
+
+}
+
